1580216cff2efeb0054e4517f69ea15b10da716e
Gareth Williams <g4z@tuta.io>
2016-11-04 11:23:22 +0100

    add installation to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# do install?
+install=0
+
+if [ ! -z "$1" ]; then
+    if [ "$1" == "--install" ]; then
+        install=1
+    fi
+fi
+
 if [ ! -d ./dist ]; then
     mkdir ./dist
 fi
@@ -12,4 +21,10 @@ if [ $? != 0 ]; then
     echo "*** ERROR: Failed creating theme zip archive ***"
 else
     echo "Theme zip archive created at $output"
+fi
+
+if [ $install == 1 ]; then
+    destination="$HOME/.config/sublime-text-3/Installed Packages/"
+    echo "Installing theme to: $destination"
+    cp -f "$output" "$destination"
 fi
